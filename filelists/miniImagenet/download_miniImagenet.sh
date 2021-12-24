29c13e165128265bdeb217abdbf4f5d5c5a13730
Yuexuan An <xiaoxiaoyuyu93@163.com>
2021-12-24 09:53:02 +0800

    Update download_miniImagenet.sh

--- filelists/miniImagenet/download_miniImagenet.sh
+++ filelists/miniImagenet/download_miniImagenet.sh
@@ -5,4 +5,3 @@ wget https://raw.githubusercontent.com/twitter/meta-learning-lstm/master/data/mi
 wget http://image-net.org/image/ILSVRC2015/ILSVRC2015_CLS-LOC.tar.gz 
 tar -zxvf ILSVRC2015_CLS-LOC.tar.gz
 python write_miniImagenet_filelist.py
-python write_cross_filelist.py
